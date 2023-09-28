5eabaa9361048b2a82d63de2b610089d4d1fede3
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-28 11:30:59 +0200

    If else block with input change

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,27 @@
 <script>
-    const name = 'Hamza'
-    const channel = '<b> Zukorlic </b>'
-    const hack = `<a href="#" onclick="alert('You have been hacked!')">Win a prize </a>`
-    const id = 'heading'
-    const disabled = false
-    const status = 'succsess'
-    let promoted = true
+    let num = 0
 </script>
 
 <main>
-    <h2 class="underline">underlined text</h2>
-    <h2 class={status}>Status</h2>
-    <!-- class:promoted adds `promoted` class if `promoted` variable is true, longer syntax is class:promoted={promoted} -->
-    <h2 class:promoted>Movie Title</h2>
-    <h1>Hello {name}!</h1>
-    <div>{@html channel}</div>
-    <div>{@html hack}</div>
-    <h2 {id}>This is a heading</h2>
-    <button {disabled} on:click={() => (promoted = !promoted)}>Button</button>
+    {#if Number(num) === 0}
+        <h2>Number is <span>zero</span></h2>
+    {:else if Number(num) < 0}
+        <h2>Number is <span>negative</span></h2>
+    {:else if Number(num) > 0}
+        <h2>Number is <span>positive</span></h2>
+    {:else}
+        <h2>Not a number</h2>
+    {/if}
+
+    <input type="text" bind:value={num} on:change={(e) => (num = e.target.value)} />
+    {#if num}
+        <p>input number is: {num}</p>
+    {:else}
+        <p>yeah</p>
+    {/if}
 </main>
 
 <style>
-    .underline {
-        text-decoration: underline;
-    }
-    .danger {
-        color: red;
-    }
-    .succsess {
-        color: olivedrab;
-    }
-    .promoted {
-        font-style: italic;
-        color: sandybrown;
-    }
     main {
         text-align: center;
         padding: 1em;
