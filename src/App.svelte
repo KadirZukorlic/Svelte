5d336b332d59d743baeebddce2c26435af738f11
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-30 19:34:23 +0200

    delete prev

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,8 @@
 <script>
-    let firstName = 'Kadir'
-    let lastName = 'Zukorlic'
-    $: fullName = `${firstName} ${lastName}`
-    $: {
-        const greet = `Full name is: ${firstName} ${lastName}`
-        console.log(greet)
-    }
 
-    let items = [
-        {
-            id: 1,
-            title: 'TV',
-            price: 100,
-        },
-        {
-            id: 2,
-            title: 'Phone',
-            price: 200,
-        },
-        {
-            id: 3,
-            title: 'MacBook Pro',
-            price: 300,
-        },
-    ]
-    $: totalAmount = items.reduce((acc, curr) => (acc = acc + curr.price), 0)
-
-    let volume = 0
-    $: if (volume < 0) {
-        alert("Can't go lower")
-        volume = 0
-    } else if (volume > 20) {
-        alert("Can't go higher")
-        volume = 20
-    }
 </script>
 
 <main>
-    <button on:click={() => (volume = volume + 1)}>Increase Volume</button>
-    <button on:click={() => (volume = volume - 1)}>Decrease Volume</button>
-    <h2>current volume {volume}</h2>
-
-    <button
-        on:click={() => {
-            ;(firstName = 'Hamza'), (lastName = 'Zukorlic')
-        }}>Change name</button
-    >
-    <h2>{fullName}</h2>
-
-    <button on:click={() => (items = [...items, { id: 4, title: 'foo', price: 50 }])}>Add item</button>
-
-    <h2>total: {totalAmount}</h2>
 </main>
 
 <style>
