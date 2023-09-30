6c840060257ffa6f4dd1856f35aaf21e0a0f6442
Kadir Zukorlic <kadirzukorlic@gmail.com>
2023-09-30 16:51:14 +0200

    Reactive declarations

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,45 @@
 <script>
+    let firstName = 'Kadir'
+    let lastName = 'Zukorlic'
+    $: fullName = `${firstName} ${lastName}`
+
+    let items = [
+        {
+            id: 1,
+            title: 'TV',
+            price: 100,
+        },
+        {
+            id: 2,
+            title: 'Phone',
+            price: 200,
+        },
+        {
+            id: 3,
+            title: 'MacBook Pro',
+            price: 300,
+        },
+    ]
+    $: totalAmount = items.reduce((acc, curr) => (acc = acc + curr.price), 0)
+    console.log('rendered', items)
 </script>
 
-<main />
+<main>
+    <button
+        on:click={() => {
+            ;(firstName = 'Hamza'), (lastName = 'Zukorlic')
+        }}>Change name</button
+    >
+    <h2>{fullName}</h2>
+
+    <button on:click={() => (items = [...items, { id: 4, title: 'foo', price: 50 }])}>Add item</button>
+
+    <h2>total: {totalAmount}</h2>
+</main>
 
 <style>
     main {
-        /* text-align: center; */
+        text-align: center;
         padding: 1em;
         max-width: 240px;
         margin: 0 auto;
